407cb8acc83fd29dc89e5063d3346ceb456eddda
abhijit.pande <pande.abhijit.v@gmail.com>
2026-01-14 10:00:34 +0530

    FIX: Metro config blockList syntax error - Use blacklistRE instead

--- metro.config.js
+++ metro.config.js
@@ -3,16 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Exclude PWA folder from bundling to reduce app size
-config.resolver = {
-  ...config.resolver,
-  blockList: [
-    ...config.resolver.blockList || [],
-    /pwa\/.*/,
-    /backend\/.*/,
-    /proxy-server\/.*/,
-    /scripts\/.*/,
-    /\.md$/,
-  ],
-};
+config.resolver.blacklistRE = /(pwa|backend|proxy-server|scripts)\/.*|.*\.md$/;
 
 module.exports = config;
